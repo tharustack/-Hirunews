f00f915bdf62a90f276f9a9129cf80bd4c460407
Tharusha Dilshan <dilshangdtd@gmail.com>
2026-01-12 18:38:22 +0530

    Update latest-news.js

--- api/latest-news.js
+++ api/latest-news.js
@@ -2,14 +2,9 @@ const HiruNewsScraper = require('../lib/scraper');
 
 module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    
-    if (req.method === 'OPTIONS') {
-        return res.status(200).end();
-    }
     
     try {
-        const { limit = 15 } = req.query;
+        const { limit = 10 } = req.query;
         const scraper = new HiruNewsScraper();
         
         const latestNews = await scraper.getLatestNews(parseInt(limit));
